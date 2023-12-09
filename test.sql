d412b0e0f8cb44ecd6394048d205788307f7ed28
nhat2105 <109486719+nhat2105@users.noreply.github.com>
2023-12-09 13:08:57 +0700

    Update test.sql newest version that include director inside crew
    Note: crew_actor not yet been changed to crew

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3305
--- Generation Time: Dec 08, 2023 at 05:37 AM
+-- Generation Time: Dec 09, 2023 at 07:05 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,6 +41,12 @@ CREATE TABLE `crew_actor` (
 
 INSERT INTO `crew_actor` (`Name`, `Bio`, `DOB`, `CID`, `Poster_URL`) VALUES
 ('Naomi Watts', 'Naomi Ellen Watts (born 28 September 1968) is a British actress. After her family moved to Australia, she made her film debut there in the drama For Love Alone (1986) and then appeared in three television series, Hey Dad..! (1990), Brides of Christ (1991), and Home and Away (1991), and the film Flirting (1991). After moving to the United States, Watts initially struggled as an actress, taking roles in small-scale films until she starred in David Lynch\'s psychological thriller Mulholland Drive in 2001 as an aspiring actress. This role started her rise to international prominence.\r\n\r\nWatts then played a tormented journalist in the horror remake The Ring (2002). She was nominated for the Academy Award for Best Actress for her performance as a grief-stricken mother in Alejandro González Iñárritu\'s film 21 Grams (2003). Her profile continued to grow with starring roles in I Heart Huckabees (2004), King Kong (2005), Eastern Promises (2007), and The International (2009).\r\n\r\nFor her role as Maria Bennett in the disaster film The Impossible (2012), Watts received another Academy Award nomination for Best Actress. In the 2010s, she starred in such films as Birdman (2014), St. Vincent (2014), While We\'re Young (2015), The Glass Castle (2017), and Luce (2019). Watts also continued to act in blockbusters, with appearances in the Divergent franchise (2015–2016), and she ventured into television with the Showtime mystery drama series Twin Peaks (2017) and the biographical limited series The Loudest Voice (2019).\r\n\r\nWatts is particularly known for her work in remakes and independent productions with dark or tragic themes, as well as portrayals of characters that endure loss or suffering.', '1968-09-28', 0, 'https://m.media-amazon.com/images/M/MV5BMjIzMjY1NTA4OF5BMl5BanBnXkFtZTcwNjk3MDYwOQ@@._V1_.jpg'),
+('David Lynch', 'David Keith Lynch is an American filmmaker, painter, visual artist, musician and actor. Lynch has received critical acclaim for his films, which are often distinguished by their surrealist qualities. He has received numerous accolades, including the Golden Lion in 2006 and an Honorary Academy Award in 2019. ', '1946-01-20', 1, 'https://m.media-amazon.com/images/M/MV5BMTQ1MTY2MTY2Nl5BMl5BanBnXkFtZTcwMDg1ODYwNA@@._V1_FMjpg_UX1000_.jpg'),
+('Robert Zemeckis', 'Robert Lee Zemeckis is an American filmmaker. He first came to public attention as the director of the action-adventure romantic comedy Romancing the Stone, the science-fiction comedy Back to the Future film trilogy, and the live-action/animated comedy Who Framed Roger Rabbit.', '1952-05-14', 2, 'https://m.media-amazon.com/images/M/MV5BMTgyMTMzMDUyNl5BMl5BanBnXkFtZTcwODA0ODMyMw@@._V1_.jpg'),
+('Steve McQueen', 'Sir Steve Rodney McQueen CBE is a British film director, film producer, screenwriter, and video artist. For services to the visual arts, he was appointed Commander of the Order of the British Empire in 2011. In 2014 he was included in Time magazine\'s annual Time 100 list of the \"most influential people in the world', '1969-10-09', 3, 'https://m.media-amazon.com/images/M/MV5BMTQzNjQ5NDUxNl5BMl5BanBnXkFtZTcwNjA0MTg1Ng@@._V1_.jpg'),
+('Alejandro González Iñárritu', 'Alejandro González Iñárritu is a Mexican filmmaker. He is primarily known for making modern psychological drama films about the human condition.', '1963-08-15', 4, 'https://variety.com/wp-content/uploads/2018/10/alejandro_gonzc3a1lez-ic3b1c3a1rritu.png'),
+('Martin Scorsese', 'Martin Charles Scorsese is an American and Italian director, producer, screenwriter and actor. He emerged as one of the major figures of the New Hollywood era', '1942-11-17', 5, 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Martin_Scorsese_MFF_2023.jpg'),
+('J.A Bayona', 'Juan Antonio García Bayona is a Spanish film director. He directed the 2007 horror film The Orphanage, the 2012 drama film The Impossible, and the 2016 fantasy drama film A Monster Calls', '1959-05-09', 6, 'https://m.media-amazon.com/images/M/MV5BMTg2MTU2ODUzOF5BMl5BanBnXkFtZTcwOTI5NjU3OA@@._V1_FMjpg_UX1000_.jpg'),
 ('Tom Hanks', 'Thomas Jeffrey Hanks (born July 9, 1956) is an American actor and filmmaker. Known for both his comedic and dramatic roles, Hanks is one of the most popular and recognizable film stars worldwide, and is widely regarded as an American cultural icon.  Hanks made his breakthrough with leading roles in the comedies Splash (1984) and Big (1988). He won two consecutive Academy Awards for Best Actor for starring as a gay lawyer suffering from AIDS in Philadelphia (1993) and a young man with below-average IQ in Forrest Gump (1994). Hanks collaborated with film director Steven Spielberg on five films: Saving Private Ryan (1998), Catch Me If You Can (2002), The Terminal (2004), Bridge of Spies (2015), and The Post (2017), as well as the 2001 miniseries Band of Brothers, which launched him as a director, producer, and screenwriter.  Hanks\' other notable films include the romantic comedies Sleepless in Seattle (1993) and You\'ve Got Mail (1998); the dramas Apollo 13 (1995), The Green Mile (1999), Cast Away (2000), Road to Perdition (2002), and Cloud Atlas (2012); and the biographical dramas Saving Mr. Banks (2013), Captain Phillips (2013), Sully (2016), and A Beautiful Day in the Neighborhood (2019). He has also appeared as the title character in the Robert Langdon film series, and has voiced Sheriff Woody in the Toy Story film series.', '1956-09-07', 80703, 'https://cdn.theatlantic.com/thumbor/doPfvTghjUX1QpglS1l35uVxZYs=/0x0:4000x5000/648x810/media/img/2023/05/02/TomHanks_Atlantic_web/original.jpg');
 
 -- --------------------------------------------------------
@@ -83,7 +89,13 @@ CREATE TABLE `crew_filmography` (
 
 INSERT INTO `crew_filmography` (`CID`, `MID`, `Role`) VALUES
 (0, 113, 'Actress'),
-(0, 159753, 'Actress');
+(0, 159753, 'Actress'),
+(1, 113, 'Director'),
+(2, 123, 'Director'),
+(3, 27194, 'Director'),
+(4, 72196, 'Director'),
+(5, 40976, 'Director'),
+(6, 159753, 'Director');
 
 -- --------------------------------------------------------
 
@@ -114,22 +126,22 @@ CREATE TABLE `movie` (
   `MID` int(255) NOT NULL,
   `Title` varchar(255) NOT NULL,
   `Year` int(255) NOT NULL,
-  `Poster_URL` varchar(255) NOT NULL,
+  `Poster` varchar(255) NOT NULL,
   `Summary` varchar(10000) NOT NULL,
-  `Director` varchar(255) NOT NULL
+  `Director` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `movie`
 --
 
-INSERT INTO `movie` (`MID`, `Title`, `Year`, `Poster_URL`, `Summary`, `Director`) VALUES
-(113, 'Mulholland Drive', 2001, 'https://m.media-amazon.com/images/I/51cDXGXoGqL._AC_UF1000,1000_QL80_.jpg', 'A dark-haired woman is left amnesiac after a car crash. She wanders the streets of Los Angeles in a daze before taking refuge in an apartment. There she is discovered by Betty, a wholesome Midwestern blonde who has come to the City of Angels seeking fame as an actress. Together, the two attempt to solve the mystery of Rita\'s true identity. The story is set in a dream-like Los Angeles, spoilt neither by traffic jams nor smog.', 'David Lynch'),
-(123, 'Forrest Gump', 1994, 'https://i.ebayimg.com/images/g/y9gAAOSwUQhi-9Nq/s-l1200.jpg', 'Slow-witted Forrest Gump has never thought of himself as disadvantaged, and thanks to his supportive mother, he leads anything but a restricted life. Whether dominating on the gridiron as a college football star, fighting in Vietnam or captaining a shrimp boat, Forrest inspires people with his childlike optimism. But one person Forrest cares about most may be the most difficult to save -- his childhood love, the sweet but troubled Jenny', 'Robert Zemeckis'),
-(27194, '12 Years a Slave', 2013, 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQUww1TfS08PJVmSN_szD3GKZ19zS-EtfBazbrlJ-ss37vISknD', 'In the years before the Civil War, Solomon Northup (Chiwetel Ejiofor), a free black man from upstate New York, is kidnapped and sold into slavery in the South. Subjected to the cruelty of one malevolent owner (Michael Fassbender), he also finds unexpected kindness from another, as he struggles continually to survive and maintain some of his dignity. Then in the 12th year of the disheartening ordeal, a chance meeting with an abolitionist from Canada changes Solomon\'s life forever.', 'Steve McQueen'),
-(40976, 'Taxi Driver', 1976, 'https://upload.wikimedia.org/wikipedia/en/3/33/Taxi_Driver_%281976_film_poster%29.jpg', 'Suffering from insomnia, disturbed loner Travis Bickle takes a job as a New York City cabbie, haunting the streets nightly, growing increasingly detached from reality as he dreams of cleaning up the filthy city. When Travis meets pretty campaign worker Betsy , he becomes obsessed with the idea of saving the world, first plotting to assassinate a presidential candidate, then directing his attentions toward rescuing 12-year-old prostitute Iris.', 'Martin Scorsese'),
-(72196, 'Babel', 2006, 'https://m.media-amazon.com/images/I/51poif023LL._AC_UF894,1000_QL80_.jpg', 'An accident connects four groups of people on three different continents: two young Moroccan goatherds, a vacationing American couple (Brad Pitt, Cate Blanchett), a deaf Japanese teen and her father, and a Mexican nanny who takes her young charges across a border without parental permission.', ' Alejandro González Iñárritu'),
-(159753, 'The Impossible', 2012, 'https://movieguide.b-cdn.net/wp-content/uploads/2013/01/p9341404_v_v8_aa.jpg', 'In December 2004, close-knit family Maria (Naomi Watts), Henry (Ewan McGregor) and their three sons begin their winter vacation in Thailand. But the day after Christmas, the idyllic holiday turns into an incomprehensible nightmare when a terrifying roar rises from the depths of the sea, followed by a wall of black water that devours everything in its path. Though Maria and her family face their darkest hour, unexpected displays of kindness and courage ameliorate their terror.', 'J. A. Bayona');
+INSERT INTO `movie` (`MID`, `Title`, `Year`, `Poster`, `Summary`, `Director`) VALUES
+(113, 'Mulholland Drive', 2001, 'https://m.media-amazon.com/images/I/51cDXGXoGqL._AC_UF1000,1000_QL80_.jpg', 'A dark-haired woman is left amnesiac after a car crash. She wanders the streets of Los Angeles in a daze before taking refuge in an apartment. There she is discovered by Betty, a wholesome Midwestern blonde who has come to the City of Angels seeking fame as an actress. Together, the two attempt to solve the mystery of Rita\'s true identity. The story is set in a dream-like Los Angeles, spoilt neither by traffic jams nor smog.', 1),
+(123, 'Forrest Gump', 1994, 'https://i.ebayimg.com/images/g/y9gAAOSwUQhi-9Nq/s-l1200.jpg', 'Slow-witted Forrest Gump has never thought of himself as disadvantaged, and thanks to his supportive mother, he leads anything but a restricted life. Whether dominating on the gridiron as a college football star, fighting in Vietnam or captaining a shrimp boat, Forrest inspires people with his childlike optimism. But one person Forrest cares about most may be the most difficult to save -- his childhood love, the sweet but troubled Jenny', 2),
+(27194, '12 Years a Slave', 2013, 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQUww1TfS08PJVmSN_szD3GKZ19zS-EtfBazbrlJ-ss37vISknD', 'In the years before the Civil War, Solomon Northup (Chiwetel Ejiofor), a free black man from upstate New York, is kidnapped and sold into slavery in the South. Subjected to the cruelty of one malevolent owner (Michael Fassbender), he also finds unexpected kindness from another, as he struggles continually to survive and maintain some of his dignity. Then in the 12th year of the disheartening ordeal, a chance meeting with an abolitionist from Canada changes Solomon\'s life forever.', 3),
+(40976, 'Taxi Driver', 1976, 'https://upload.wikimedia.org/wikipedia/en/3/33/Taxi_Driver_%281976_film_poster%29.jpg', 'Suffering from insomnia, disturbed loner Travis Bickle takes a job as a New York City cabbie, haunting the streets nightly, growing increasingly detached from reality as he dreams of cleaning up the filthy city. When Travis meets pretty campaign worker Betsy , he becomes obsessed with the idea of saving the world, first plotting to assassinate a presidential candidate, then directing his attentions toward rescuing 12-year-old prostitute Iris.', 5),
+(72196, 'Babel', 2006, 'https://m.media-amazon.com/images/I/51poif023LL._AC_UF894,1000_QL80_.jpg', 'An accident connects four groups of people on three different continents: two young Moroccan goatherds, a vacationing American couple (Brad Pitt, Cate Blanchett), a deaf Japanese teen and her father, and a Mexican nanny who takes her young charges across a border without parental permission.', 4),
+(159753, 'The Impossible', 2012, 'https://movieguide.b-cdn.net/wp-content/uploads/2013/01/p9341404_v_v8_aa.jpg', 'In December 2004, close-knit family Maria (Naomi Watts), Henry (Ewan McGregor) and their three sons begin their winter vacation in Thailand. But the day after Christmas, the idyllic holiday turns into an incomprehensible nightmare when a terrifying roar rises from the depths of the sea, followed by a wall of black water that devours everything in its path. Though Maria and her family face their darkest hour, unexpected displays of kindness and courage ameliorate their terror.', 6);
 
 -- --------------------------------------------------------
 
@@ -319,6 +331,12 @@ ALTER TABLE `crew_actor`
 --
 ALTER TABLE `crew_awards`
   ADD PRIMARY KEY (`Name`,`Year`);
+
+--
+-- Indexes for table `crew_filmography`
+--
+ALTER TABLE `crew_filmography`
+  ADD PRIMARY KEY (`CID`,`MID`,`Role`);
 
 --
 -- Indexes for table `critic`
